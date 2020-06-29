37f23540f2cb087cfbfddb652cd14ce14c1a9a8e
Maksym-777 <64743459+Maksym-777@users.noreply.github.com>
2020-06-29 17:16:33 +0200

    adaptiv 29.06.20
    adaptiv 29.06.20

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const burgerMenu = document.querySelector(".menu-mobile__block");
 const menu = document.querySelector(".menu-mobile__list");
-const closeMenu = document.querySelector(".menu-mobile__item--close");
+const closeMenu = document.querySelector(".menu-mobile__close");
 const menuMobile = document.querySelector(".menu-mobile");
 
 burgerMenu.addEventListener('click', function (e) {
+  e.preventDefault();
   menuMobile.style.display = 'block';
 });
 
 closeMenu.addEventListener('click', function (e) {
+  e.preventDefault();
   menuMobile.style.display = 'none';
+  
 });

068020180e55a9fcb73ca8a8b7a1bbd2c9ad6bb2
Maksym-777 <64743459+Maksym-777@users.noreply.github.com>
2020-07-05 11:23:48 +0200

    Add files via upload
    modal

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+/* ------------ modal -------------- */
+
+$('.form').submit(e => {
+    e.preventDefault()
+  
+    $.fancybox.open({
+      src: "#modal",
+      type: "inline",
+
+    })
+  })
--- js/script.js
+++ js/script.js
@@ -15,3 +15,33 @@ closeMenu.addEventListener('click', function (e) {
   menuMobile.style.visibility = 'hidden';
 
 });
+
+$(document).ready(function () {
+  $('.team__name').click(function () {
+    $(this).next().slideToggle("slow");
+    $(this).toggleClass('team__name--active')
+
+    /* $(this).next().toggleClass('team__accordion--active');
+    $(this).toggleClass('team--active') */
+  });
+})
+
+
+$(document).ready(function () {
+  $('.slider__image').slick({
+    nextArrow: document.querySelector('#my-arrow-next'),
+    prevArrow: document.querySelector('#my-arrow-prev')
+  });
+});
+
+
+
+$(".reviews__item").not(":first").hide();
+
+$(".reviews .reviews__switcher-item").click(function () {
+  $(".reviews .reviews__switcher-item").removeClass("reviews__switcher-item--active").eq($(this).index()).addClass("reviews__switcher-item--active");
+  $(".reviews__item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("reviews__switcher-item--active");
+
+
+
